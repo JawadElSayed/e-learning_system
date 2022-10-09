c5bfb40363d166cf81c0f7a2abe2d813c71e784b
JawadElSayed <jawadelsayed3@gmail.com>
2022-10-09 13:12:28 +0300

    backend > creating models

---
+++ e-learning_server/app/Models/Assignment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Assignment extends Model {
+
+    use HasFactory;
+    protected $connection = 'mongodb';
+    protected $collection = 'assignments';
+
+    protected $fillable = [
+        'course_code',
+        'user_id',
+        'title',
+        'assignment',
+        'due_to',
+    ];
+}
