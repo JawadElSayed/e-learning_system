ff7370b78ebc173764640c213d3a4ada00779a03
JawadElSayed <jawadelsayed3@gmail.com>
2022-10-10 00:17:41 +0300

    backend > adding login api

--- e-learning_server/routes/api.php
+++ e-learning_server/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get("/add_user_type/{user_type}", [AdminController::class, "addUserType"]);
 Route::post("/add_user", [AdminController::class, "addUser"])->name("add-user");
+Route::post("/login", [AuthController::class, "login"])->name("login");
