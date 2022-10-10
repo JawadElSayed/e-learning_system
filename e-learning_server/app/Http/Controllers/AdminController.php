4c523e1107f54e913222bcd32a2b1ffdd8383550
JawadElSayed <jawadelsayed3@gmail.com>
2022-10-10 16:51:39 +0300

    backend > fixing admin controller

--- e-learning_server/app/Http/Controllers/AdminController.php
+++ e-learning_server/app/Http/Controllers/AdminController.php
@@ -9,7 +9,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
-use PhpParser\Node\Stmt\Return_;
 
 class AdminController extends Controller {
 
@@ -17,44 +16,44 @@ class AdminController extends Controller {
 
         // validating inputes
         $request->validate([
-                'name' => 'required|string|max:255',
-                'user_type' => 'required|string',
-            ]);
-            // generating password
-            $password = Str::random(8);
-            
-            // specifying user type
-            $user_type = UserType::where("user_type", $request->user_type)->first();
-            if ($user_type === null){
-                return response()->json([
-                    'status' => 'error',
-                    'message' => 'user type does not exist',
-                ]);
-            }
-            $user_type = $user_type["id"];
-
-            // getting user id
-            $user_id = User::orderByDesc("user_id")->select("user_id")->first();
-            if ($user_id === null){
-                $user_id = 10000;
-            }
-            else{
-                $user_id = $user_id["user_id"] + 1;    
-            }
-            
-            $user = User::create([
-                'user_id' => $user_id,
-                'name' => $request->name,
-                'password' => Hash::make($password),
-                'user_type' => $user_type,
-            ]);
-
+            'name' => 'required|string|max:255',
+            'user_type' => 'required|string',
+        ]);
+        // generating password
+        $password = Str::random(8);
+        
+        // specifying user type
+        $user_type = UserType::where("user_type", $request->user_type)->first();
+        if ($user_type === null){
             return response()->json([
-                'status' => 'success',
-                'message' => 'User created successfully',
-                'user' => $user,
-                'password' => $password,
+                'status' => 'error',
+                'message' => 'user type does not exist',
             ]);
+        }
+        $user_type = $user_type["id"];
+
+        // getting user id
+        $user_id = User::orderByDesc("user_id")->select("user_id")->first();
+        if ($user_id === null){
+            $user_id = 10000;
+        }
+        else{
+            $user_id = $user_id["user_id"] + 1;    
+        }
+        
+        $user = User::create([
+            'user_id' => $user_id,
+            'name' => $request->name,
+            'password' => Hash::make($password),
+            'user_type' => $user_type,
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User created successfully',
+            'user' => $user,
+            'password' => $password,
+        ]);
     }
 
     public function addUserType($user_type){
