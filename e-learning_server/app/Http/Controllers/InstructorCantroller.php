185fb86ee6e74eb731e14723c9b20b046dadefe9
JawadElSayed <jawadelsayed3@gmail.com>
2022-10-10 22:55:34 +0300

    backend > fixing validation inputes

--- e-learning_server/app/Http/Controllers/InstructorCantroller.php
+++ e-learning_server/app/Http/Controllers/InstructorCantroller.php
@@ -6,17 +6,26 @@ use App\Models\Assignment;
 use App\Models\Course;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class InstructorCantroller extends Controller {
 
     public function addAssignment(Request $request) {
 
-        $request->validate([
-            'course_id' => 'required|string',
-            'title' => 'required|string',
-            'assignment' => 'required|string',
-            'due_to' => 'required|date',
-        ]);
+        // validating inputes
+        try {
+            $request->validate([
+                'course_id' => 'required|string',
+                'title' => 'required|string',
+                'assignment' => 'required|string',
+                'due_to' => 'required|date',
+            ]);
+        } catch (ValidationException $exception) {
+            return response()->json([
+                'status' => 'error',
+                'errors' => $exception->getMessage(),
+            ]);
+        }
 
         $course_exist = Course::find($request->course_id);
         if($course_exist === null){
@@ -44,10 +53,18 @@ class InstructorCantroller extends Controller {
 
     public function addAnnouncement(Request $request) {
 
-        $request->validate([
-            'title' => 'required|string',
-            'announcement' => 'required|string',
-        ]);
+        // validating inputes
+        try {
+            $request->validate([
+                'title' => 'required|string',
+                'announcement' => 'required|string',
+            ]);
+        } catch (ValidationException $exception) {
+            return response()->json([
+                'status' => 'error',
+                'errors' => $exception->getMessage(),
+            ]);
+        }
 
         $user_id = Auth::user();
         $assignment = Assignment::create([
