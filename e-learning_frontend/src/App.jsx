6e493f80f47945fcca4a68fe79c9b8a00d620844
JawadElSayed <jawadelsayed3@gmail.com>
2022-10-11 23:55:44 +0300

    frontend > adding page to app

--- e-learning_frontend/src/App.jsx
+++ e-learning_frontend/src/App.jsx
@@ -1,20 +1,41 @@
-import { useState } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import LeftBar from "./componetes/LeftBar";
+import Page from "./componetes/Page";
+import axios from "axios";
 
 function App() {
     const [page, setPage] = useState("");
+    const [students, setStudents] = useState([]);
 
+    useEffect(() => {
+        fetchStudents();
+    }, []);
+
+    // const fetchStudents = async () => {
+    //     const res = await fetch("http://127.0.0.1:8000/students");
+    //     const data = await res.json();
+    //     console.log(data);
+    //     return data;
+    // };
+    const fetchStudents = () => {
+        axios
+            .get("http://127.0.0.1:8000/students")
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
     return (
         <div className="app-div">
-            <LeftBar userType={"admin"} setPage={setPage} />
+            <LeftBar userType={"instructor"} setPage={setPage} />
             <div className="page-container">
                 <h1>{page}</h1>
                 <div>
-                    {page === "Instructors" && "Hello from this component"}
-                    {page === "Courses" && "Hello from courses component"}
-                    {page === "Students" && "Hello from studentß component"}
-                    {page === "Instructors" && "Hello from this component"}
+                    <Page pageTitle={page} />
                 </div>
             </div>
         </div>
